0cfe3049d45a122bc90880b4eed5a67b7657f28a
GDVG <guillermovalleg@gmail.com>
2018-03-01 17:03:11 +0100

    Updated install.sh, added link check and changed the symlink to absolute path

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo ln -s decoder.py /usr/bin/decoder
+cwd=$(pwd)
+
+if [ ! -L /usr/bin/decoder ];
+then
+	sudo ln -s $cwd/decoder.py /usr/bin/decoder
+else
+	echo "El programa decoder ya está instalado, elimínalo de /usr/bin"
+fi
